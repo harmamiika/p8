6d3d6969acd87a1b72620bd5130f0602a072cdb0
Miika Härmä <harmamiika@gmail.com>
2021-10-22 19:09:50 +0300

    8.8 done

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from '..';
 
 const Authors = (props) => {
+  const response = useQuery(ALL_AUTHORS);
+
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  if (response.loading) {
+    return <div>loading-</div>;
+  }
+
+  const authors = response.data.allAuthors;
 
   return (
     <div>
@@ -13,25 +23,20 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {authors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -1,5 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+client.query({ query: ALL_AUTHORS }).then((response) => {
+  console.log(response.data);
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
